038166586535e4c51bdb1d5053c96395bc04eefe
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-04-19 20:27:27 -0400

    feat: Display a list of cat names and images
    Modify script.js to display a list of cats names and images in a flexbox container.

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-const catImage = document.getElementById('cat-picture');
-const counterDiv = document.getElementById('output-div');
-let catClicks = 0;
+const cats = [
+  {name: "Mittens",
+   image: "./cat1.jpg"},
+  {name: "Snowball",
+   image: "./cat2.jpg"},
+]
 
-catImage.addEventListener('click', function(){
-  catClicks++;
-  counterDiv.innerText = catClicks;
-})
+
+function displayCat(cat){
+  // create local variables for the properties of the cat object
+  let name = cat.name;
+  let imagePath = cat.image;
+
+  // create a div to hold each cat's info
+  let catBox = document.createElement('div');
+  catBox.classList.add('cat-container');
+
+  // create the heading with the cats's name
+  let catName = document.createElement('h2');
+  catName.classList.add('catHeading')
+  catName.textContent = name;
+
+  // create the image element
+  let catPic = document.createElement('img');
+  catPic.src = imagePath;
+
+  // append the elements onto the div that will contain the cat
+  catBox.appendChild(catName);
+  catBox.appendChild(catPic);
+
+  // return the div to be appended to the outer container
+  return catBox;
+}
+
+const outerContainer = document.getElementById('outerContainer');
+
+
+for (let cat of cats) {
+  outerContainer.appendChild(displayCat(cat));
+}
